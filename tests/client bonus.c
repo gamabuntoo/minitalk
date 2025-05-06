987db32abea892b67236428c48886d9291043047
Guillaume Le Batard <gule-bat@paul-f4Ar1s2.clusters.42paris.fr>
2025-05-06 21:51:21 +0200

    testsfile

---
+++ tests/client bonus.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gule-bat <gule-bat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 15:23:44 by gule-bat          #+#    #+#             */
+/*   Updated: 2025/05/06 21:40:18 by gule-bat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "./libft/libft.h"
+
+void	errorprint(void)
+{
+	ft_putstr_fd("Error PID", 2);
+	exit(EXIT_FAILURE);
+}
+
+void	receiver(int signum, siginfo_t *info, void *context)
+{
+	(void)signum;
+	(void)context;
+	ft_putstr_fd("Message received by server (pid = ", 1);
+	ft_putnbr_fd(info->si_pid, 1);
+	ft_putstr_fd(")\n", 1);
+	// exit(EXIT_SUCCESS);
+}
+
+int	ft_isdigit_check(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (ft_isdigit(str[i]) == 1)
+			i++;
+		else
+			return (-1);
+	}
+	return (1);
+}
+
+int	str_to_bit(char *str, pid_t pid)
+{
+	unsigned int		i;
+	int j;
+	char	c;
+
+	c = 0;
+	j = -1;
+	while (str[++j] != '\0')
+	{
+		i = 8;
+		c = str[j];
+		while (i--)
+		{
+			if (c >> i & 1)
+			{	
+				if (kill(pid, SIGUSR1) == -1)
+					return (ft_putstr_fd("Error PID", 2), 0);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					return (ft_putstr_fd("Error PID", 2), 0);
+			}
+			pause();
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		pause();
+		kill(pid, SIGUSR2);
+	}
+	exit(EXIT_SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	signals;
+
+	if (argc != 3 || ft_isdigit_check(argv[1]) == -1)
+		return (ft_putstr_fd("Minitalk Client:\n\t./client serverpid message\n", 1), -1);
+	if (ft_atoi(argv[1]) == -1)
+		return (ft_putstr_fd("Minitalk Client:\n\t./client serverpid message\n", 1), -1);
+	signals.sa_sigaction = receiver;
+	sigemptyset(&signals.sa_mask);
+	signals.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &signals, NULL);
+	str_to_bit(argv[2], ft_atoi(argv[1]));
+	return (0);
+}
---
+++ tests/server bonus.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gule-bat <gule-bat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 23:41:26 by gule-bat          #+#    #+#             */
+/*   Updated: 2025/05/06 21:41:09 by gule-bat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "./libft/libft.h"
+
+void	receiver(int signum, siginfo_t *info, void *context)
+{
+	static char	c;
+	static int	i;
+	static int	ct;
+	pid_t	pid;
+	
+	(void)context;
+	c <<= 1;
+	pid = info->si_pid;
+	if (signum == SIGUSR1)
+	{
+		c |= 1;
+		ct = 0;
+	}
+	if (signum == SIGUSR2)
+		ct++;
+	kill(pid, SIGUSR1);
+	i++;
+	if (i == 8)
+	{
+		if (ct == 8)
+		{
+			kill(pid, SIGUSR1);
+			ft_putstr_fd("\n", 1);
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		i = 0;
+	}
+}
+
+int	main(void) 
+{
+	struct sigaction	signals;
+	pid_t				pid;
+
+	pid = getpid();
+	signals.sa_sigaction = receiver;
+	sigemptyset(&signals.sa_mask);
+	signals.sa_flags = SA_SIGINFO;	
+
+	printf("PID SERVER: %d\n", pid);
+
+	sigaction(SIGUSR1, &signals, NULL);
+	sigaction(SIGUSR2, &signals, NULL);
+	printf("Pause...\n");
+	while (1)
+		pause();
+	return 0;
+}
